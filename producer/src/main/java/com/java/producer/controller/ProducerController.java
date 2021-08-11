6bd8ca3a0080e7ca70947bf875fe025d15e00754
sarawutklu <sarawut.klu@gmail.com>
2021-08-11 12:48:17 +0700

    add config con && pro

--- producer/src/main/java/com/java/producer/controller/ProducerController.java
+++ producer/src/main/java/com/java/producer/controller/ProducerController.java
@@ -1,5 +1,7 @@
 package com.java.producer.controller;
 
+import com.java.producer.services.Producer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class ProducerController {
 
+    private final Producer producer;
+
+    @Autowired
+    ProducerController(Producer producer) {
+        this.producer = producer;
+    }
+    
+
    @PostMapping("/{message}")
    public String postMethodName(@RequestBody String message) {
-       return String.format("push message %s success!", message);
+        this.producer.sendMessage(message);
+        return String.format("push message %s success!", message);
    }
    
     
